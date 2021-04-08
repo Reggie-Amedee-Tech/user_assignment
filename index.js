dd1a58d83fb8387c451e0b3ced3fc06016d42de5
Reggie Amedee <jeantechnical1992@gmail.com>
2021-04-07 22:47:57 -0400

    user_assignment

---
+++ index.js
@@ -0,0 +1,45 @@
+class user {
+    constructor(userName) {
+        this.name = userName
+        this.balance = 0
+    }
+    makeDeposits(amount) {
+        this.balance += amount
+    }
+    makeWithdrawel(amount) {
+        this.balance -= amount
+    }
+    displayBalance() {
+        return this.balance
+    }
+    transferMoney(otherUser, amount) {
+        this.balance = this.balance - amount
+        otherUser.balance = otherUser.balance + amount
+    }
+}
+
+const reggie = new user('Reginald Jean Amedee')
+const moneifa = new user('Moneifa Cherice Nance')
+const betty = new user('Betty Noel')
+
+reggie.makeDeposits(1000)
+reggie.makeDeposits(1000)
+reggie.makeDeposits(1000)
+reggie.makeWithdrawel(500)
+reggie.displayBalance()
+
+moneifa.makeDeposits(2000)
+moneifa.makeDeposits(2000)
+moneifa.makeWithdrawel(200)
+moneifa.makeWithdrawel(200)
+moneifa.displayBalance()
+
+betty.makeDeposits(5000)
+betty.makeWithdrawel(300)
+betty.makeWithdrawel(500)
+betty.makeWithdrawel(200)
+betty.displayBalance()
+
+reggie.transferMoney(betty, 1000)
+
+console.log(reggie)
